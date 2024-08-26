7ea897f9d72ab1980f52978c56a7659781ba8ab0
mariambingoo <mariamsheta13@gmail.com>
2024-08-26 14:55:03 +0300

    11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,6 +4,6 @@
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  const arr = process.argv.slice(2).sort();
+  const arr = process.argv.slice(2).map(Number).sort((a, b) => a - b);
   console.log(arr.reverse()[1]);
 }
