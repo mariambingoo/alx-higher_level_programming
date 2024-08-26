6aa0d2a5738f977c4219a86cce0b5e2dfe0d6f2e
mariambingoo <mariamsheta13@gmail.com>
2024-08-26 14:31:36 +0300

    10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// computes and prints a factorial
+
+function fac (a) {
+  if (isNaN(a) || a === 1) {
+    return 1;
+  } else {
+    return a * fac(a - 1);
+  }
+}
+
+console.log(fac(parseInt(process.argv[2])));
